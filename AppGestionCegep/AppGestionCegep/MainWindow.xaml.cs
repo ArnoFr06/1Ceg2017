9a318ec770bbcf970f76baf170c4b006f50293c4
VictorCORU <21500238@etu.unicaen.fr>
2017-04-18 14:31:12 -0400

    modif main + xml

--- AppGestionCegep/AppGestionCegep/MainWindow.xaml.cs
+++ AppGestionCegep/AppGestionCegep/MainWindow.xaml.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml;
+
 
 namespace AppGestionCegep
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+    
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -27,7 +19,6 @@ namespace AppGestionCegep
             InitializeComponent();
             // hello world
             Cours a = new Cours("oui", "oui", 15);
-            button.Content = a.getNum();
             Console.WriteLine(Width);
 
             // tab1.Width = Width; 
@@ -42,10 +33,10 @@ namespace AppGestionCegep
             this.Title = "Clicked";
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.FileName = "Document"; // Default file name
-            dlg.DefaultExt = ".text"; // Default file extension
+            dlg.DefaultExt = ".txt"; // Default file extension
             dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
             
-                        // Show save file dialog box
+            // Show save file dialog box
             Nullable <bool> result = dlg.ShowDialog();
 
             // Process save file dialog box results
@@ -57,10 +48,11 @@ namespace AppGestionCegep
             string lines = "First line.\r\nSecond line.\r\nThird line.";
 
             // Write the string to a file.
-            System.IO.StreamWriter file = new System.IO.StreamWriter(".\test.txt");
-            file.WriteLine(lines);
-            file.Close();
 
+           // StreamWriter file = new StreamWriter();
+           // file.WriteLine(lines);
+           // file.Close();
+            
         }
 
         private void TabItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
